1f5213c4d21bbfe6b61ea2266e0a1ed8cb299ffa
Vadim Krasovsky <vadzimkrasouski@gmail.com>
2021-12-08 18:12:51 +0100

    -added createPost and onePost

--- components/CreatePost.tsx
+++ components/CreatePost.tsx
@@ -1,36 +1,77 @@
 import { CREATE_POST } from '../pages/api/posts';
 import { useMutation } from '@apollo/client';
-import React, { useState } from 'react';
+import React, { ChangeEvent, useState } from 'react';
+import styled from 'styled-components';
+
+const Input = styled.input.attrs(props => ({
+    type: 'text',
+    size: props.size || '1em',
+    required: props.required,
+}))`
+  border: 2px solid palevioletred;
+  margin: ${props => props.size};
+  padding: ${props => props.size};
+`;
+const Button = styled.button.attrs(props => ({
+    type: 'submit',
+    disabled: props.disabled,
+}))`
+  display: inline-block;
+  color: palevioletred;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
 
 export const CreatePost = () => {
-    const [newPost, {loading, data}] = useMutation(CREATE_POST);
+    const [newPost, {loading}] = useMutation(CREATE_POST);
     const [title, setTitle] = useState('');
-    const [body, setText] = useState('');
-    const addPost = (e: React.SyntheticEvent) => {
-        e.preventDefault()
-        newPost({
-            variables: {
-                input: {
-                    title, body
+    const [body, setBody] = useState('');
+    const [status, setStatus] = useState<string | null>(null);
+
+    const addPost = () => {
+        if (title.trim() && body !== '') {
+            newPost({
+                variables: {
+                    input: {
+                        title, body
+                    }
                 }
-            }
-        }).then(({data}) => {
-            console.log(data)
-            setTitle('')
-            setText('')
-        })
+            }).then(({data}) => {
+                console.log(data)
+                setTitle('')
+                setBody('')
+                setStatus('DONE')
+            })
+        } else {
+            setStatus('Title and body are required');
+        }
     }
 
+    const onKeyPressHandler = (e: KeyboardEvent) => {
+        if (status !== null) {
+            setStatus(null)
+        }
+        if (e.key === 'Enter') {
+            addPost();
+        }
+    }
 
     return (
         <div>
-            <form>
-                <input placeholder='Title' value={title} onChange={e => setTitle(e.target.value)} type='text'/>
-                <input placeholder='write a text here' value={body} onChange={e => setText(e.target.value)}
-                       type='text'/>
-                <button disabled={loading} onClick={(e) => addPost(e)}>Create</button>
-                {data ? 'DONE' : undefined}
-            </form>
+            <Input placeholder='Title'
+                   value={title}
+                   onChange={(e: ChangeEvent<HTMLInputElement>) => setTitle(e.currentTarget.value)}
+                   onKeyPress={onKeyPressHandler}/>
+            <Input placeholder='write a text here'
+                   value={body}
+                   onChange={(e: ChangeEvent<HTMLInputElement>) => setBody(e.currentTarget.value)}
+                   onKeyPress={onKeyPressHandler}
+            />
+            <Button disabled={loading} onClick={addPost}>Create</Button>
+            {status}
         </div>
     );
 };
--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,12 +1,28 @@
 import { Layout } from '../../layouts/Layout';
 import { useRouter } from 'next/router';
+import { useQuery } from '@apollo/client';
+import { GET_ONE_POST } from '../api/posts';
 
 export default function Post() {
     const router = useRouter()
     const {id} = router.query
+    console.log(id)
+    const {loading, error, data} = useQuery(GET_ONE_POST, {
+        variables: {postId: id}
+    });
+
+    if (error)
+        return <div>Error loading posts.</div>;
+    if (loading)
+        return <div>Loading</div>;
+
+    const {post} = data;
+    console.log(post)
+
     return (
         <Layout pageTitle={`Post: ${id}`}>
-            <h1>Post: {id}</h1>
+            <h1>Post: {post.title}</h1>
+            <p>{post.body}</p>
         </Layout>
     );
 };
